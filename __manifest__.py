c57220b985cf98b5ac76ce0638cf1d74c92efc72
Germán Ponce Dominguez <german.ponce@argil.mx>
2022-12-05 13:26:17 -0600

    Actualizaciones

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Análisis de Ventas vs Facturas',
+    'version': '1.0',
+    'category': 'Reportes',
+    'description': """
+
+        Este modulo agrega un Analisis de Ventas vs Facturas.
+
+
+    """,
+    'author': 'PonceSoft',
+    'website': 'http://poncesoft.blogspot.com',
+    'depends': ['base','sale','account'],
+    'data': [
+        'extra_fit_views.xml',
+    ],
+    'installable': True,
+    'active': False,
+    'certificate' : False,
+}
